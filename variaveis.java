2d73649190fca9e020a3096d324e3d0926b934fd
G2biSstrey <gabrielle.strey@outlook.com>
2025-02-25 09:20:27 -0300

    variaves_java

---
+++ variaveis.java
@@ -0,0 +1,26 @@
+package aula_1;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//variáveis servem para armazenar valores
+		
+		String nome= "ALUNO";
+		int idade= 20;
+		double altura= 1.60;
+		
+			
+		//saida de dados
+		System.out.println("BEM VINDO(A) " + nome);
+		System.out.println("Sua altura é de " + altura);
+		System.out.println("Sua idade informada é de " + idade);
+		
+	
+	
+		
+		
+	}
+
+}
